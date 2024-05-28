a564294d0fe35b1ba19e82732d5f84251bf84ae6
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2024-05-29 00:11:48 +0530

    Update POM xml  File and add main packages

---
+++ src/test/java/com/dlad/qa/base/BaseClass.java
@@ -0,0 +1,74 @@
+package com.dlad.qa.base;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import com.dlad.qa.utils.Utils;
+
+public class BaseClass {
+	
+//	Create Reusable Method
+	
+	WebDriver driver;
+	public Properties prop;
+	
+//	Create Load Properties File for Config Main before Browser Initialize
+	public void loadPropertiesFile(){
+		
+		// Main Properties
+		prop = new Properties();
+		File propFile = new File(System.getProperty("user.dir")+"\\src\\main\\java\\com\\dlad\\qa\\config\\config.properties");
+		
+		FileInputStream fils;
+		try {
+			fils = new FileInputStream(propFile);
+			prop.load(fils);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+//	Initializing the Browser
+	
+	public WebDriver initializeBrowser(String browserName) {			
+		
+		if(browserName.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+			
+		}else if(browserName.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
+			
+		}else if(browserName.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+			
+		}else if(browserName.equalsIgnoreCase("safari")){
+			driver = new SafariDriver();
+			
+		}else {
+			System.out.println("Browser Not Found... Try Again!");
+
+		}
+				
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Utils.IMPLICIT_WAIT_TIME));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(Utils.PAGE_LOAD_TIME));
+		
+		driver.get(prop.getProperty("url"));
+		
+		return driver;
+	}
+
+}
