806324026edddb00ce97efa1dda90cb71a26cc0f
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2024-07-18 10:33:04 +0530

    Update Config Files

--- src/main/java/com/dlad/qa/listeners/DLADCRMListeners.java
+++ src/main/java/com/dlad/qa/listeners/DLADCRMListeners.java
@@ -4,10 +4,7 @@ import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
 
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.io.FileHandler;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
