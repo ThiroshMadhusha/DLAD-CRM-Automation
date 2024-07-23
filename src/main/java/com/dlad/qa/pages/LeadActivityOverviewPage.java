8e7fe1a8b042f9ca68456708f36e759e3b824aff
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2024-07-23 22:30:42 +0530

    Update Lead Activity Overview Page

--- src/main/java/com/dlad/qa/pages/LeadActivityOverviewPage.java
+++ src/main/java/com/dlad/qa/pages/LeadActivityOverviewPage.java
@@ -1,5 +1,6 @@
 package com.dlad.qa.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -7,15 +8,96 @@ import org.openqa.selenium.support.PageFactory;
 
 public class LeadActivityOverviewPage {
 	
-	WebDriver driver;
+    WebDriver driver;
+
+    public LeadActivityOverviewPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+    
+    /**
+     * Locators
+     */
+    
+	@FindBy(xpath = "//div[@class='flex gap-3 items-center']/h1")
+	private WebElement LeadActivityPageTitle;
 	
-	public LeadActivityOverviewPage(WebDriver driver) {
-		this.driver = driver;
-		PageFactory.initElements(driver, this);
-}
+	@FindBy(xpath = "//h3[contains(text(), 'Dead')]")
+	private WebElement DeadLabel;
 	
-	@FindBy(xpath = "")
-	private WebElement WebPageLogin;
+	@FindBy(xpath = "//h3[contains(text(), 'Converted')]")
+	private WebElement ConvertedLabel;
+	
+	@FindBy(xpath = "//h3[contains(text(), 'Account Application Received')]")
+	private WebElement AccountApplicationReceiveLabel;
+	
+	@FindBy(xpath = "//h3[contains(text(), 'Pricing')]")
+	private WebElement PricingLabel;
+	
+	@FindBy(xpath = "//h3[contains(text(), '4-Account Application Sent')]")
+	private WebElement AccountApplicationSentLabel;
+	
+	@FindBy(xpath = "//h3[contains(text(), 'Sampling')]")
+	private WebElement SamplingLabel;
+	
+	@FindBy(xpath = "//h3[contains(text(), 'Contact Made')]")
+	private WebElement ContactMadeLabel;
+	
+	@FindBy(xpath = "//h3[contains(text(), 'New')]")
+	private WebElement NewLabel;
+
+   
+	
+    public String getCount1(WebElement labelElement) {
+        WebElement parentDiv = labelElement.findElement(By.xpath("./ancestor::div[@class='rounded-xl border bg-card text-card-foreground shadow']"));
+        WebElement countElement = parentDiv.findElement(By.xpath(".//div[@class='text-2xl font-bold']"));
+        return countElement.getText();
+    }
+    
+	public String LeadActivityPageTitle() {
+		String leadHeaderTitleName = LeadActivityPageTitle.getText();
+		return leadHeaderTitleName;
+	}
+	
+	public String DeadLabel() {
+		String DeadLabelName = DeadLabel.getText();
+		return DeadLabelName;
+	}
+	
+	public String ConvertedLabel() {
+		String DeadLabelName = ConvertedLabel.getText();
+		return DeadLabelName;
+	}
+	
+	public String AccountApplicationReceiveLabel() {
+		String AccountApplicationReceiveLabelName = AccountApplicationReceiveLabel.getText();
+		return AccountApplicationReceiveLabelName;
+	}
+	
+	public String PricingLabel() {
+		String PricingLabelName = PricingLabel.getText();
+		return PricingLabelName;
+	}
+	
+	public String AccountApplicationSentLabel() {
+		String AccountApplicationSentLabelName = AccountApplicationSentLabel.getText();
+		return AccountApplicationSentLabelName;
+	}
+	
+	public String SamplingLabel() {
+		String SamplingLabelName = SamplingLabel.getText();
+		return SamplingLabelName;
+	}
+	
+	public String ContactMadeLabel() {
+		String ContactMadeLabelName = ContactMadeLabel.getText();
+		return ContactMadeLabelName;
+	}
+	
+	public String NewLabel() {
+		String NewLabelName = NewLabel.getText();
+		return NewLabelName;
+	}
 	
 
 }
